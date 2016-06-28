ae0a301ad65ca3d67ea98bff3e7a7dbd749ac281
Xiaopo Li <lixiaopo@chexiang.com>
2016-06-28 15:06:42 +0800

    commit test project

---
+++ TestCocoaPods/TestCocoaPods/TestCocoaPods.h
@@ -0,0 +1,15 @@
+//
+//  TestCocoaPods.h
+//  TestCocoaPods
+//
+//  Created by lixiaopo on 16/6/28.
+//  Copyright © 2016年 XiaopoLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestCocoaPods : NSObject
+
++ (void)test;
+
+@end
---
+++ TestCocoaPods/TestCocoaPods/TestCocoaPods.m
@@ -0,0 +1,18 @@
+//
+//  TestCocoaPods.m
+//  TestCocoaPods
+//
+//  Created by lixiaopo on 16/6/28.
+//  Copyright © 2016年 XiaopoLi. All rights reserved.
+//
+
+#import "TestCocoaPods.h"
+
+@implementation TestCocoaPods
+
++ (void)test
+{
+    NSLog(@"TestCocoaPods test");
+}
+
+@end
